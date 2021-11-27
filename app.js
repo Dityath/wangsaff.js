a1a209538410c5bf3d7ba0d00b71e6c8ef05e083
dityath <dityaathallah107@gmail.com>
2021-11-28 05:03:14 +0700

    adding nodemon and developer tools, fix docker again, adding session

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const { Client } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
-// const fs = require("fs");
+const fs = require("fs");
 
-// const SESSION_FILE_PATH = "./whatsapp-session.json";
-
-// let sessionCfg;
-// if (fs.existsSync(SESSION_FILE_PATH)) {
-//   sessionCfg = require(SESSION_FILE_PATH);
-// }
+const SESSION_FILE_PATH = "./whatsapp-session.json";
+let sessionCfg;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+  sessionCfg = require(SESSION_FILE_PATH);
+}
 
 const client = new Client({
   puppeteer: {
     headless: true,
     // browserWSEndpoint: `ws://178.128.25.31:1000`,
   },
-  // session: sessionCfg,
+  session: sessionCfg,
 });
 
 client.on("qr", (qr) => {
@@ -25,9 +24,11 @@ client.on("qr", (qr) => {
 client.on("authenticated", (session) => {
   console.log("AUTHENTINCATED", session);
   sessionCfg = session;
-  // fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
-  //   err ? console.log(err) : "";
-  // });
+  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
 });
 
 client.on("ready", () => {
@@ -38,10 +39,11 @@ client.on("message", async (msg) => {
   const chat = await msg.getChat();
   const contact = await msg.getContact();
 
-  await chat.sendMessage(`Hello @${contact.id.user}`, {
-    mentions: [contact],
-  });
-
+  if (msg.body === "!nama") {
+    await chat.sendMessage(`Hello ${contact.id.user}`, {
+      mentions: [contact],
+    });
+  }
 });
 
 client.initialize();
